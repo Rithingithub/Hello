0e673cd8ba03349753369d666c2e2f56c8693e57
Rithin Lehan <74766580+Rithingithub@users.noreply.github.com>
2021-09-07 17:02:25 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,9 @@
-print ("Kiddo you may hacked");
+print ("Hello World");
+
+f= open("Dont touch.txt","w+")
+
+
+for i in range(10):
+     f.write("Kiddo you may hacked %d\r\n" % (i+1))
+    
+f.close()
